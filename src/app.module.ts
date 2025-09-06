c2c8b6043d0100ea2d4b2ab4af0427b39a9ab173
iphone <jomia156@gmail.com>
2025-09-06 19:09:50 +0300

    generate auth and server modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import {LoggerModule} from "./components/logger/logger.module"
 import { PeerModule } from './peer/peer.module';
 import { WgcoreModule } from './components/wgcore/wgcore.module';
 import {ConfigModule} from "@nestjs/config"
+import { ServerModule } from './server/server.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),PeerModule, WgcoreModule, LoggerModule, PrismaModule],
-})
+  imports: [ConfigModule.forRoot({isGlobal:true}),PeerModule, WgcoreModule, LoggerModule, PrismaModule, ServerModule, AuthModule],
+  providers: []
+  })
 export class AppModule {}
